b9569f508538006617c2fa4f13b87970433ebfd9
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2022-01-26 19:04:19 -0800

    newkey

--- client/functions/getTodos.js
+++ client/functions/getTodos.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 exports.handler = async (event) =>{
-    const GET_LINKS = `
+    const GET_TODOS = `
         query{
             allTodos{
                 data {
@@ -13,5 +13,23 @@ exports.handler = async (event) =>{
                 }
             }
         }`;
+  
+    const {data} = await axios({
+        url: 'https://graphql.fauna.com/graphql',
+        method: 'post',
+        headers: {     
+            Authorization: `Bearer fnAEd6QNH6AASd4d4P17t4UPSxL_-Oxjgeyt6Vq7`
+        },
+        data: {
+            query: GET_TODOS,
+            variables: {},
+        }
+    })
+    console.log(data);
 
+    return {
+        statusCode:200,
+        body: JSON.stringify(data)
+    }
+   
 }
